3c480cdef756d252f483af93fdd4ca4ac51c2d97
Theo Paris <theop@flowtr.dev>
2024-05-09 15:08:38 -0700

    chore: initial commit

---
+++ test/read_zero_size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "test_util.h"
+
+OPTNONE int main(void) {
+  char *p = malloc(0);
+  if (!p) {
+    return 1;
+  }
+  printf("%c\n", *p);
+  return 0;
+}
---
+++ util.c
@@ -0,0 +1,44 @@
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <unistd.h>
+
+#ifdef __ANDROID__
+#include <async_safe/log.h>
+int mallopt(int param, int value);
+#define M_BIONIC_RESTORE_DEFAULT_SIGABRT_HANDLER (-1003)
+#endif
+
+#include "util.h"
+
+#ifndef __ANDROID__
+static int write_full(int fd, const char *buf, size_t length) {
+  do {
+    ssize_t bytes_written = write(fd, buf, length);
+    if (bytes_written == -1) {
+      if (errno == EINTR) {
+        continue;
+      }
+      return -1;
+    }
+    buf += bytes_written;
+    length -= bytes_written;
+  } while (length);
+
+  return 0;
+}
+#endif
+
+COLD noreturn void fatal_error(const char *s) {
+#ifdef __ANDROID__
+  mallopt(M_BIONIC_RESTORE_DEFAULT_SIGABRT_HANDLER, 0);
+  async_safe_fatal("hardened_malloc: fatal allocator error: %s", s);
+#else
+  const char *prefix = "fatal allocator error: ";
+  (void)(write_full(STDERR_FILENO, prefix, strlen(prefix)) != -1 &&
+         write_full(STDERR_FILENO, s, strlen(s)) != -1 &&
+         write_full(STDERR_FILENO, "\n", 1));
+  abort();
+#endif
+}
